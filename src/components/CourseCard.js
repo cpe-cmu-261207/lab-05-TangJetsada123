c83284b58abc7f473d879fe17141442973e4aa26
เจษฎา จินะกะ <Jetsada_Jinaka@hotmail.com>
2021-03-15 17:06:16 +0700

    ver1

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,9 +1,9 @@
-const CourseCard = (props) => {
-  return (
-    <>
-      <p>This is Course card</p>
-    </>
-  );
+export const CourseCard = (props) => {
+  // TODO: design HTML component that displays a course as a "card" on the webpage.
+  return <>
+  <p>{props.name}</p>
+  <p>{props.grd}</p>
+  <p>{props.crd}</p>
+  <button onClick = {() =>props.del(props.name)} > x </button>
+  </>;
 };
-
-export default CourseCard;
